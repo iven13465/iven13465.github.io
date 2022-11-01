b12bd7273fa53bb47b16c409c2d31bf65f343935
iven13465 <3191980057@qq.com>
2022-11-01 20:18:48 +0800

    Site updated: 2022-11-01 20:18:44

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "30df9ff67662452594d71f2ce3bcb54a"
+    }
+  }
---
+++ status/config.js
@@ -0,0 +1,27 @@
+
+window.Config = {
+
+  // 显示标题
+  SiteName: '233',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1896205-df2b4e3e31bf4aef7e63be2f'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Home',
+      url: 'https://iven233.com'
+    },
+
+  ],
+};
